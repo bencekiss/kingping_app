e012969c0f4c028c6ed9326c25db245e13547a4c
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-30 14:35:00 -0500

    added controllers and first two players

---
+++ db/migrate/20170130191810_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :players do |t|
+      t.string :nickname
+      t.string :name
+      t.integer :wins
+      t.integer :losses
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
