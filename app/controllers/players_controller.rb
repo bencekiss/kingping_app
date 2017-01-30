f152d3fb9205afe01b4981488611f4fa5554c59a
Dgibbins <nayr_ryan@hotmail.com>
2017-01-30 18:23:43 -0500

    Controller functions added

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,20 +8,40 @@ class PlayersController < ApplicationController
   end
 
   def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to '/players'
+    else
+      render :new
+    end
   end
 
   def edit
+    @player= Player.find(params[:id])
+    render :edit
   end
 
   def update
+    @player = Player.find(params[:id])
+    if @player.update_attributes(player_params)
+      redirect_to "/players/#{player.id}"
+    else
+      render :edit
+    end
   end
 
   def show
-
+    @player = Player.find(params[:id])
   end
 
   def destroy
-
+    @player = Player.find(params[:id])
+    @player.destroy
+    redirect_to players_url
   end
 
+  private
+  def player_params
+    params.require(:player).permit(:nickname, :name, :wins, :losses, :url)
+  end
 end
