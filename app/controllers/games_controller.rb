c2cb66032e18f972f487cd73357186bc8a9c9ce1
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-30 19:22:23 -0500

    created the edit method and show page. update still missing.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,13 +9,31 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    @game_player1 = GamePlayer.new(player1_params)
-    @game_player2 = GamePlayer.new(player2_params)
+    @game_player1 = GamePlayer.new(@game.id, @game.player1_id)
+    @game_player2 = GamePlayer.new(@game.id, @game.player2_id)
     if @game.save
       redirect_to games_url
     else
       redirect_to new_game_url
     end
+  end
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update_attributes(games_params)
+      redirect_to "/games/#{@game.id}"
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    @player1 = Player.find(@game.player1_id)
+    @player2 = Player.find(@game.player2_id)
   end
 
   private
@@ -23,10 +41,5 @@ class GamesController < ApplicationController
   def game_params
     params.require(:game).permit(:player1_id, :player2_id, :gamedate, :p1points, :p2points)
   end
-  def player1_params
-    params.require(:game).permit(:id, :player1_id)
-  end
-  def player2_params
-    params.require(:game).permit(:id, :player2_id)
-  end
+
 end
--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -1,4 +1,9 @@
 class GamePlayer < ApplicationRecord
   belongs_to :player
   belongs_to :game
+
+  def initialize(game_id, player_id)
+    @game_id = game_id
+    @player_id = player_id
+  end
 end
